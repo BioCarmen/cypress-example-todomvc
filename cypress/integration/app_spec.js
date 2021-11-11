28276d724125879eb53ad5e057580fe2f23e88cf
carmen <carmen@biorender.com>
2021-11-11 14:58:26 -0500

    d

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -40,6 +40,7 @@ describe("TodoMVC - React", function () {
     // which is automatically prepended to cy.visit
     //
     // https
+    //
     // ://on.cypress.io/api/visit
     cy.visit("/");
   });

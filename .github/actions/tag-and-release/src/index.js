aaa4d5e3aba89e14ee887f637d266fd97cef13d0
carmen <carmen@biorender.com>
2022-01-13 15:03:25 -0500

    test

--- .github/actions/tag-and-release/src/index.js
+++ .github/actions/tag-and-release/src/index.js
@@ -6,10 +6,7 @@ const action = async () => {
   const token = core.getInput("github-token", { required: true });
   const client = new Octokit({ auth: token });
 
-  const labels = context.payload.pull_request.labels;
-  const labelsName = labels.map((label) => label.name);
   const repo = context.payload.repository;
-  const labelsToCheck = core.getInput("label", { required: true }).split(",");
 
   core.debug(repo);
   const tags = await client.repos.listTags({

68da98411c720c4e6fa85aad6a9332f7950f2252
carmen <carmen@biorender.com>
2022-01-14 10:37:00 -0500

    test

--- release.js
+++ release.js
@@ -1,10 +1,15 @@
+const { exec } = require("child_process");
 module.exports = async ({ github, context, core }) => {
   const today = new Date();
   console.log(today);
+  exec("git describe --tags `git rev-list --tags --max-count=1`");
+  //   creare ref
   //    github.rest.git.createRef({
   //         owner: context.repo.owner,
   //          repo: context.repo.repo,
   //          ref: `refs/tags/${{steps.generate_tag.outputs.new_tag}}`,
   //          sha: context.sha
   //        })
+
+  //   Create a release
 };

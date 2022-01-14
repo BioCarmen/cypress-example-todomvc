320a53cb5d14e33875f8773b0b7002c0675fb6fc
carmen <carmen@biorender.com>
2022-01-14 10:22:03 -0500

    release

--- release.js
+++ release.js
@@ -1,21 +1,9 @@
-import { Octokit } from "@octokit/core";
-const { context } = require("@actions/github");
-
-const action = async () => {
-  const token = core.getInput("github-token", { required: true });
-  const client = new Octokit({ auth: token });
-
-  const repo = context.payload.repository;
-  try {
-    console.log(repo);
-    const tags = await client.repos.listTags({
-      ...context.repo,
-      per_page: 100,
-      page: 1,
-    });
-    core.debug(tags);
-  } catch (error) {
-    core.debug(error);
-  }
+module.exports = async ({ github, context, core }) => {
+  const { SHA } = process.env;
+  const commit = await github.rest.repos.getCommit({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: `${SHA}`,
+  });
+  core.exportVariable("author", commit.data.commit.author.email);
 };
-action();

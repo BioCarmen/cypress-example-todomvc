1d9ac327d609e7fb32a31177002767e65a2124a3
carmen <carmen@biorender.com>
2022-01-14 11:27:28 -0500

    test

--- release.js
+++ release.js
@@ -1,42 +1,41 @@
 const { exec } = require("child_process");
-module.exports = async ({ github, context, core }) => {
+const eee = async () => {
   let newTagName = "";
   const today = new Date().toISOString().split("T")[0];
   console.log(today);
 
-  const { stdout, stderr } = await exec(
-    "git tag --sort=committerdate | tail -1"
-  );
+  exec("git tag --sort=committerdate | tail -1", async (error, stdout) => {
+    if (error) {
+      console.error(`exec error: ${JSON.stringify(error)}`);
+      return;
+    }
 
-  if (stderr) {
-    console.error(`exec error: ${stderr}`);
-    return;
-  }
+    const latestVersion = stdout.trim();
 
-  const latestVersion = stdout.trim();
+    const latestVersionItems = latestVersion.split("-");
+    const latestVersionDate = `${latestVersionItems[1]}-${latestVersionItems[2]}-${latestVersionItems[3]}`;
+    console.log(
+      "latesetst",
+      latestVersion,
+      latestVersionDate,
+      latestVersionDate === today
+    );
+    if (latestVersionDate === today) {
+      newTagName = `prod-${latestVersionItems}-${latestVersionItems[4] + 1}`;
+    } else {
+      newTagName = `prod-${today}-01`;
+    }
+    console.log(newTagName);
+    await console.log("don't start");
+    //  creare ref
+    await github.rest.git.createRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: `refs/tags/${newTagName}`,
+      sha: context.sha,
+    });
 
-  const latestVersionItems = latestVersion.split("-");
-  const latestVersionDate = `${latestVersionItems[1]}-${latestVersionItems[2]}-${latestVersionItems[3]}`;
-  console.log(
-    "latesetst",
-    latestVersion,
-    latestVersionDate,
-    latestVersionDate === today
-  );
-  if (latestVersionDate === today) {
-    newTagName = `prod-${latestVersionItems}-${latestVersionItems[4] + 1}`;
-  } else {
-    newTagName = `prod-${today}-01`;
-  }
-  console.log(newTagName);
-
-  //  creare ref
-  await github.rest.git.createRef({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `refs/tags/${newTagName}`,
-    sha: context.sha,
+    //   Create a release
   });
-
-  //   Create a release
 };
+eee();

5cb4f259faac234e72d637e043365515d1a4079d
BioCarmen <carmen@biorender.com>
2022-01-25 11:16:07 -0500

    rwar

--- release.js
+++ release.js
@@ -3,7 +3,8 @@ module.exports = async ({ github, context, core }) => {
   let newTagName = "";
   const today = new Date().toISOString().split("T")[0];
   execSync("git fetch --prune --tags");
-  const latestTag = execSync("git describe --tags --abbrev=0");
+  const rev = execSync(`git rev-list --tags --max-count=1`);
+  const latestTag = execSync(`git describe --tags ${rev}`);
   console.log("latest", latestTag);
   // exec("git describe --tags --abbrev=0", async (error, stdout) => {
   //   console.log("lssss", stdout);

cfe0c34c8729a65e76409cdd538ba1f5fbdc8b4f
BioCarmen <carmen@biorender.com>
2022-01-25 11:09:42 -0500

    test

--- release.js
+++ release.js
@@ -3,51 +3,51 @@ module.exports = async ({ github, context, core }) => {
   let newTagName = "";
   const today = new Date().toISOString().split("T")[0];
   execSync("git fetch --prune --tags");
+  execSync("git describe --tags --abbrev=0");
+  // exec("git describe --tags --abbrev=0", async (error, stdout) => {
+  //   console.log("lssss", stdout);
+  //   if (error) {
+  //     console.error(`exec error: ${JSON.stringify(error)}`);
+  //     return;
+  //   }
 
-  exec("git describe --tags --abbrev=0", async (error, stdout) => {
-    console.log("lssss", stdout);
-    if (error) {
-      console.error(`exec error: ${JSON.stringify(error)}`);
-      return;
-    }
+  //   const latestVersion = stdout.trim();
 
-    const latestVersion = stdout.trim();
+  //   const latestVersionItems = latestVersion.split("-");
+  //   const latestVersionDate = `${latestVersionItems[1]}-${latestVersionItems[2]}-${latestVersionItems[3]}`;
+  //   const latestVersionNum = parseInt(latestVersionItems[4]) + 1;
 
-    const latestVersionItems = latestVersion.split("-");
-    const latestVersionDate = `${latestVersionItems[1]}-${latestVersionItems[2]}-${latestVersionItems[3]}`;
-    const latestVersionNum = parseInt(latestVersionItems[4]) + 1;
+  //   if (latestVersionDate === today) {
+  //     const newNum = parseInt(latestVersionItems[4]) + 1;
+  //     newTagName = `prod-${latestVersionDate}-${newNum}`;
+  //   } else {
+  //     newTagName = `prod-${today}-1`;
+  //   }
 
-    if (latestVersionDate === today) {
-      const newNum = parseInt(latestVersionItems[4]) + 1;
-      newTagName = `prod-${latestVersionDate}-${newNum}`;
-    } else {
-      newTagName = `prod-${today}-1`;
-    }
+  //   //  creare ref
+  //   try {
+  //     // await github.rest.git.createRef({
+  //     //   owner: context.repo.owner,
+  //     //   repo: context.repo.repo,
+  //     //   ref: `refs/tags/${newTagName}`,
+  //     //   sha: context.sha,
+  //     // });
 
-    //  creare ref
-    try {
-      // await github.rest.git.createRef({
-      //   owner: context.repo.owner,
-      //   repo: context.repo.repo,
-      //   ref: `refs/tags/${newTagName}`,
-      //   sha: context.sha,
-      // });
+  //     core.exportVariable("author", newTagName);
 
-      core.exportVariable("author", newTagName);
-
-      //   //   Create a release
-      //   await github.rest.repos.createRelease({
-      //     owner: context.repo.owner,
-      //     repo: context.repo.repo,
-      //     tag_name: newTagName,
-      //   });
-      // execSync(`git fetch && git checkout release`);
-      // execSync(`git reset --hard ${newTagName}`);
-      // execSync("git push -f");
-    } catch (error) {
-      // execSync(`git push --delete origin ${newTagName}`);
-      console.error(error);
-      process.exit(1);
-    }
-  });
+  //     //   //   Create a release
+  //     //   await github.rest.repos.createRelease({
+  //     //     owner: context.repo.owner,
+  //     //     repo: context.repo.repo,
+  //     //     tag_name: newTagName,
+  //     //   });
+  //     // execSync(`git fetch && git checkout release`);
+  //     // execSync(`git reset --hard ${newTagName}`);
+  //     // execSync("git push -f");
+  //   } catch (error) {
+  //     // execSync(`git push --delete origin ${newTagName}`);
+  //     console.error(error);
+  //     process.exit(1);
+  //   }
+  // });
 };

77e30c6c206a701e445e65958e8bcfa6e4c82547
BioCarmen <carmen@biorender.com>
2022-02-28 13:08:46 -0500

    test

--- cypress/integration/anther_spec.js
+++ cypress/integration/anther_spec.js
@@ -61,7 +61,7 @@ describe("TodoMVC - React", function () {
   it("adds 2 todos", function () {
     cy.get(".new-todo").type("learn testing{enter}").type("be cool{enter}");
 
-    cy.get(".todo-list li").should("have.length", 2);
+    cy.get(".todo-list li").should("have.length", 7);
   });
 
   context("When page is initially opened", function () {
